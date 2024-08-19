c44076ecc5bbbf38f37f202f9c4cdcc82442c9c9
Momen Sameh <95236373+Mo2men-dev@users.noreply.github.com>
2024-08-19 20:48:33 +0300

    task_2: edit

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
-"""a module
+"""Task 2: Hypermedia pagination
 """
+
 import csv
 import math
-from typing import List, Tuple, Dict
+from typing import Dict, List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page and page size.
+    """
+
+    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
 
 
 class Server:
@@ -26,53 +34,27 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Retrieves a page of data.
+        """
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
-        self.dataset()
-        t = index_range(page, page_size)
-        if t[0] > len(self.__dataset):
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
             return []
-        if t[0] * t[1] > len(self.__dataset):
-            return self.__dataset[t[0]:]
-        return self.__dataset[t[0]:t[1]]
-        # assert type(page) == int and type(page_size) == int
-        # assert page > 0 and page_size > 0
-        # start, end = index_range(page, page_size)
-        # data = self.dataset()
-        # if start > len(data):
-        #     return []
-        # return data[start:end]
-        # assert type(page) is int and page > 0
-        # assert type(page_size) is int and page_size > 0
-
-        # indexes = index_range(page, page_size)
-        # try:
-        #     data = self.dataset()
-        #     return data[indexes[0]: indexes[1]]
-        # except IndexError:
-        #     return []
+        return data[start:end]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
-        myDict = {}
-        listy = self.get_page(page, page_size)
-        myDict["page_size"] = len(listy)
-        myDict["page"] = page
-        myDict["data"] = listy
-        myDict["next_page"] = page + 1 if page * page_size < len(
-            self.__dataset) else None
-        myDict["prev_page"] = page - 1 if not page == 1 else None
-        myDict["total_pages"] = int(
-            len(self.__dataset) / page_size) + 1 if not len(
-                self.__dataset) % page_size == 0 else len(
-                    self.__dataset) / page_size
-        return myDict
-
-
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """index range
-    """
-    if page == 1:
-        return (0, page_size)
-    return ((page * page_size) - page_size, page * page_size)
-    # index_tuple = page_size * (page - 1), page * page_size
-    # return index_tuple
+        """Retrieves information about a page.
+        """
+        data = self.get_page(page, page_size)
+        start, end = index_range(page, page_size)
+        total_pages = math.ceil(len(self.__dataset) / page_size)
+        return {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': page + 1 if end < len(self.__dataset) else None,
+            'prev_page': page - 1 if start > 0 else None,
+            'total_pages': total_pages
+        }
