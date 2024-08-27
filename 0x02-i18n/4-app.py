e4247c0825db7e5fc6dbe7279ecda1b7de8321c2
Mo2men-dev <momensameh5@gmail.com>
2024-08-27 22:17:25 +0000

    0x02-i18n: task_4/fix

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -28,8 +28,8 @@ def get_locale() -> str:
     Returns:
         str: best match
     """
-    locale = request.args.get('locale', '').strip()
-    if locale and locale in Config.LANGUAGES:
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
